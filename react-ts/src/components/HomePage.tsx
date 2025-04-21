1994792211e501ba7bd13ad515fa6369c8d38225
Arnav Gupta <guptaarn@msu.edu>
2025-04-21 15:20:40 -0400

    basic update

--- react-ts/src/components/HomePage.tsx
+++ react-ts/src/components/HomePage.tsx
@@ -10,10 +10,16 @@ export default function HomePage() {
         <section id="hero">
           <Hero />
         </section>
+        <section id="skills" style={{ paddingTop: '100px' }}>
+          <Experience />
+        </section>
         <section id="journey" style={{ paddingTop: '200px' }}>
           <Journey />
         </section>
         <section id="experience" style={{ paddingTop: '100px' }}>
+          <Experience />
+        </section>
+        <section id="projects" style={{ paddingTop: '100px' }}>
           <Experience />
         </section>
       </>
